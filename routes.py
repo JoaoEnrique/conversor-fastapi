2acd3b90e8149c9fb569f82f4abe0689b4814648
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-03-31 17:49:02 -0300

    feat: v2 com validação utilizando pydantic

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from asyncio import gather
 from fastapi import APIRouter, Path, Query
 from converter import sync_converter, async_converter
+from schamas import ConverterInput
 
 router = APIRouter()
 
@@ -46,4 +47,46 @@ async def async_converter_router(
         coroutines.append(coro)
         
     result = await gather(*coroutines)
+    return result
+
+
+@router.get("/v2/converter/{from_currency}")
+def converter(
+    body: ConverterInput,
+    from_currency: str = Path(max_length=3, regex="^[A-Z]{3}$"),
+):
+    to_currencies = body.to_currencies
+    price = body.price
+    
+    result = []
+    for currency in to_currencies:
+        response = sync_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+        
+        result.append(response)
+        
+    return result
+
+
+@router.get("/v2/async/converter/{from_currency}")
+def converter(
+    body: ConverterInput,
+    from_currency: str = Path(max_length=3, regex="^[A-Z]{3}$"),
+):
+    to_currencies = body.to_currencies
+    price = body.price
+    
+    result = []
+    for currency in to_currencies:
+        response = sync_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+        
+        result.append(response)
+        
     return result
---
+++ schamas.py
@@ -0,0 +1,25 @@
+"""
+{
+    "price": 123,
+    "to_currencies": ['USD', 'BRL']
+}
+"""
+
+from pydantic import BaseModel, Field, validator
+from typing import list
+import re
+
+class ConverterInput(BaseModel):
+    price: float = Field(gt=0)
+    to_currencies: list[str]
+    
+    @validator("to_currencies")
+    def validate_to_currencies(cls, value):
+        for currency in value:
+            if not re.match('^[A-Z]{3}$', currency):
+                raise ValueError("Invalid currency {currency}")
+            
+        return value
+    
+    
+    
